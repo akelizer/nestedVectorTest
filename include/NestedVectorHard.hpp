c7b229074ab3c41afa63780aceb5262a697c9f46
john <akelizer@gmail.com>
2023-10-16 08:28:28 -0400

    adding more functions to reserve() and including them in header file. Rescursive findAvailableNode() now operates with a for loop.

--- include/NestedVectorHard.hpp
+++ include/NestedVectorHard.hpp
@@ -6,11 +6,13 @@
 
 class NestedVectorHard {
 private:
-    size_t m_depth;// Indicates the height of the tree.
-    // TODO: Add data structures here!
-    std::vector <double> v;
-    double m_data[];
-
+    /**
+    size_t m_depth; // Indicates the height of the tree. where are pointers being directed? The leaves or a node?
+    size_t m_level;
+    std:: vector<double> m_data;
+    std:: vector<int> m_tree;
+    **/
+    
 public:
     NestedVectorHard(size_t depth);
 
@@ -20,6 +22,20 @@ public:
     double get(const TensorIndices& indices);
     void reserve(size_t level, size_t count);
     void append(double data);
+
+    size_t findAvailableNode(size_t index = 0);
+    bool nodeIsAvailable(size_t index);
+    void incrementNodeSize(size_t index);
+    void setAddressOfLeaf(size_t index);
+    void setAddressOfChild(size_t index);
+    void appendNewNodeToMainTree(size_t index);
+
+
+
+    size_t m_depth; // Indicates the height of the tree. where are pointers being directed? The leaves or a node?
+    size_t m_level;
+    std:: vector<double> m_data;
+    std:: vector<int> m_tree;
 };
 
 
--- src/NestedVectorHard.cpp
+++ src/NestedVectorHard.cpp
@@ -15,28 +15,59 @@ double NestedVectorHard::get(const TensorIndices& indices) {
 
 void NestedVectorHard::reserve(size_t level, size_t count) {
     
-    // check if branching is allowed on current tree level
+    /**
+     * // check if branching is allowed on current tree level
     if (m_depth - 1 <= level)
         m_level = level; // update current level on tree
     else   
         return error "out of depth";
+    **/
+
+
+   if (m_tree[0] == 0) // or if m_tree[0] == null || if (m_tree.length() == null)? // some way to prove no node has been created yet.
+      appendNewNodeToMainTree(count);
+    else {
+        size_t availableNodeIndex = findAvailableNode();
+        incrementNodeSize(availableNodeIndex);
+        setAddressOfChild(availableNodeIndex);
+        appendNewNodeToMainTree(count);
+        }
     }
+    
 
 // new version of findAvailableNode(). Returns the starting index of the next available node in m_tree
-auto NestedVectorHard::findAvailableNode(size_t index) {
+
+size_t NestedVectorHard::findAvailableNode(size_t index){
+    if (nodeIsAvailable(index))
+        return index;
+    else 
+        size_t firstChild = index + 2;
+        size_t lastChild = index + m_tree[index] + 1;
+        for (size_t childIndex = firstChild; childIndex < lastChild; childIndex++)
+            findAvailableNode(m_tree[childIndex]);
+
+}
+
+bool NestedVectorHard::nodeIsAvailable(size_t index){
+    if (m_tree[index+1] < m_tree[index])
+        return true;
+}
+
+/**auto NestedVectorHard::findAvailableNode(size_t index) {
     if (m_tree[index+1] < m_tree[index])
         return index;
     else 
         index += m_tree[index]+2;
-        findAvailableNode(index);
+        findAvailableNode(index);   // forwards the function to search the first address of the next node.
     }
-
+**/
 
     //indexType AvailableNodeIndex = findNextAvailableNode(startOfTree) 
         // recursive function to find the next available node to branch from in m_tree and return its starting index value. The available node must not be filled to capacity.
     
-    // updates values in node to reflect increase in its size.
-void NestedVectorHard::incrementNodeSize(AvailableNodeIndex)
+// updates values in node to reflect increase in its size.
+
+void NestedVectorHard::incrementNodeSize(size_t AvailableNodeIndex)
 {
     m_tree[AvailableNodeIndex+1] += 1; // increment size value in available node by 1.
 }
@@ -44,10 +75,16 @@ void NestedVectorHard::incrementNodeSize(AvailableNodeIndex)
 
 
 // new version of setAddressOfChild().
-auto NestedVectorHard::setAddressOfChild(size_t index) {
-    size_t remainingBranches = m_tree[index] - m_tree[index+1]
+void NestedVectorHard::setAddressOfChild(size_t index) {
+    size_t remainingBranches = m_tree[index] - m_tree[index+1];
     size_t indexOfAvailableBranch = m_tree.length() - remainingBranches;
     m_tree[indexOfAvailableBranch] = m_tree.size()+1; // the address of the child node will be one index past the current m_tree size.
+}
+
+void NestedVectorHard::setAddressOfLeaf(size_t availableNodeIndex) {
+    size_t remainingBranches = m_tree[index] - m_tree[index+1];
+    size_t indexOfAvailableBranch = m_tree.length() - remainingBranches;
+    m_tree.insert(indexOfAvailableBranch, m_data.size()-1) // stores the index of m_data[] into the appropriate node value within m_tree
 }
 
 /**
@@ -63,9 +100,9 @@ sets the child node offset value within the available node.
 }  
 **/ 
 
-void NestedVectorHard::appendNewNodeToMainTree(capacity) {
-    // build out empty vector to hold node's data.
-    std::vector<size_t> node(count+2);    // creates node with [0,0,0,0,0], pehaps with some constructor.
+// build out empty vector to hold node's data.
+void NestedVectorHard::appendNewNodeToMainTree(size_t capacity) {
+    std::vector<size_t> node(capacity+2);    // creates node with [0,0,0,0,0], pehaps with some constructor.
 
     // fills the vector with 0s on first initialzation except the first index.
     for (auto nodeIterator : node)
@@ -75,22 +112,15 @@ void NestedVectorHard::appendNewNodeToMainTree(capacity) {
     node[0] = capacity;
 
 
-    // appends new node values to m_tree. Maybe put this routine towards the bottom after main tree has updated its child offsets. 
+    // appends new node values to m_tree.
     for (auto nodeIterator : node)
         m_tree.push_back(nodeIterator);
-
-
-
-
-    delete node;
-
 }
 
 void NestedVectorHard::append(double data) {
-
     m_data.push_back(data); // append m_data vector with a value provided
-    m_tree.insert(currentNodeAndBranch(), m_data.size()-1) // stores the index of m_data[] into the appropriate node value within m_tree
-
+    size_t availableNodeIndex = findAvailableNode();
+    setAddressOfLeaf(availableNodeIndex);
 }
 
 size_t NestedVectorHard::depth() {
